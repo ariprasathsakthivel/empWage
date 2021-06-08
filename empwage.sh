96e89112a8fa8373cf50d21bb57a80b43ead5289
unknown <ariprasath3@gmail.com>
2021-06-08 20:12:12 +0530

    added: daily employee wage

--- empwage.sh
+++ empwage.sh
@@ -4,6 +4,11 @@ randomcheck=$((RANDOM%2));
 if [ $present -eq $randomcheck ];
 then
 		echo "Employee is present";
+		emprateperhr=20;
+		emphrs=8;
+		salary=$(($emphrs*$emprateperhr));
+		echo "Salary of the employee is $salary";
 else
 		echo "Employee is absent";
+		echo "salary is 0";
 fi
