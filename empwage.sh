61b966bc4c916d04a645869c056434a0dfd9b31c
unknown <ariprasath3@gmail.com>
2021-06-08 20:33:12 +0530

    added: wage calculation using switch case

--- empwage.sh
+++ empwage.sh
@@ -3,15 +3,16 @@ parttime=1;
 fulltime=2;
 emprateperhr=20;
 randomcheck=$((RANDOM%3));
-if [ $fulltime -eq $randomcheck ];
-then
-		echo "Employee is fulltime";
-		emphrs=8;
-elif [ $parttime -eq $fulltime ];
-then
-		emphrs=4;
-else
-		emphrs=0;
-		fi
+case $randomcheck in
+	$fulltime)
+		emphrs=8
+		;;
+	$partime)
+		emphrs=4
+		;;
+	*)
+		emphrs=0
+		;;
+esac
 salary=$(($emphrs*$emprateperhr));
 echo "salary of the employee is $salary";
