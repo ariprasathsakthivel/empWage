0fbb1f3c481aa7de4c010efe39309067a7b2aa42
unknown <ariprasath3@gmail.com>
2021-06-08 20:28:45 +0530

    added: part time wage

--- empwage.sh
+++ empwage.sh
@@ -1,14 +1,17 @@
 #! /bin/bash -x
-present=1;
-randomcheck=$((RANDOM%2));
-if [ $present -eq $randomcheck ];
+parttime=1;
+fulltime=2;
+emprateperhr=20;
+randomcheck=$((RANDOM%3));
+if [ $fulltime -eq $randomcheck ];
 then
-		echo "Employee is present";
-		emprateperhr=20;
+		echo "Employee is fulltime";
 		emphrs=8;
-		salary=$(($emphrs*$emprateperhr));
-		echo "Salary of the employee is $salary";
+elif [ $parttime -eq $fulltime ];
+then
+		emphrs=4;
 else
-		echo "Employee is absent";
-		echo "salary is 0";
-fi
+		emphrs=0;
+		fi
+salary=$(($emphrs*$emprateperhr));
+echo "salary of the employee is $salary";
