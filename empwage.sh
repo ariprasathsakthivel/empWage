c4c4fb1214432ff9c2307e916469a4020500ed17
unknown <ariprasath3@gmail.com>
2021-06-08 19:58:54 +0530

    added: attendence check

--- empwage.sh
+++ empwage.sh
@@ -1,1 +1,9 @@
-#Welcome to employee wage computation program on master branch
+#! /bin/bash -x
+present=1;
+randomcheck=$((RANDOM%2));
+if [ $present -eq $randomcheck ];
+then
+		echo "Employee is present";
+else
+		echo "Employee is absent";
+fi
