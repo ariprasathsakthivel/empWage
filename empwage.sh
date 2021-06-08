0b4b9b8e7e677dde289b0349e1f640feb5f709f0
unknown <ariprasath3@gmail.com>
2021-06-08 20:49:23 +0530

    added: calculate wages using functions

--- empwage.sh
+++ empwage.sh
@@ -1,31 +1,28 @@
-  
 #! /bin/bash -x
-#constants
 full_time=1;
 part_time=2;
 emp_rate_per_hr=20;
 num_of_working_days=20;
 max_hrs_in_month=100;
-
 #variable
 total_emp_hrs=0;
 total_working_days=0;
-while [ $total_emp_hrs -lt $max_hrs_in_month -a $total_working_days -lt $num_of_working_days ]
-do
-	((total_working_days++))
-	emp_check=$((RANDOM%3)) #generating a random number
-	case $emp_check in
-		$full_time)
-			emp_hrs=8
+function getworkinghours() {
+	case $1 in
+		$full_time) emp_hrs=8
 		;;
-		$part_time)
-			emp_hrs=4
+		$part_time) emp_hrs=4
 		;;
-		*)
-			emp_hrs=0
+		*) emp_hrs=0
 		;;
 	esac
-total_emp_hrs=$(($total_emp_hrs+$emp_hrs));
+	echo $emp_hrs;
+}
+
+while [ $total_emp_hrs -lt $max_hrs_in_month -a $total_working_days -lt $num_of_working_days ]
+do
+	total_working_days=$(($total_working_days+1));
+	work_hrs="$( getworkinghours $((RANDOM%3)) )";
+	total_emp_hrs=$(($total_emp_hrs+$work_hrs));
 done
-wage=$(($emp_rate_per_hr*$total_emp_hrs));
-echo "$wage";
+wage=$(($total_emp_hrs*$emp_rate_per_hr));
