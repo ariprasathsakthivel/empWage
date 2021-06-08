3d4cac0d1187170d17491fd3baaca0a76e092a5a
unknown <ariprasath3@gmail.com>
2021-06-08 20:46:18 +0530

    added: Calculate Wages till

--- empwage.sh
+++ empwage.sh
@@ -1,13 +1,18 @@
+  
 #! /bin/bash -x
 #constants
 full_time=1;
 part_time=2;
 emp_rate_per_hr=20;
-num_of_working_days=30
-total_salary=0
+num_of_working_days=20;
+max_hrs_in_month=100;
+
 #variable
-for ((day=1; day<=$num_of_working_days; day++))
+total_emp_hrs=0;
+total_working_days=0;
+while [ $total_emp_hrs -lt $max_hrs_in_month -a $total_working_days -lt $num_of_working_days ]
 do
+	((total_working_days++))
 	emp_check=$((RANDOM%3)) #generating a random number
 	case $emp_check in
 		$full_time)
@@ -20,7 +25,7 @@ do
 			emp_hrs=0
 		;;
 	esac
-wage=$(($emp_rate_per_hr*$emp_hrs));
-total_salary=$(($total_salary+wage));
-echo "$total_salary";
+total_emp_hrs=$(($total_emp_hrs+$emp_hrs));
 done
+wage=$(($emp_rate_per_hr*$total_emp_hrs));
+echo "$wage";
