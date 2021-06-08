0e2ffc84ba382c93aeda836d2828e7d09f052560
unknown <ariprasath3@gmail.com>
2021-06-08 20:42:34 +0530

    added: calculating wages per month

--- empwage.sh
+++ empwage.sh
@@ -1,18 +1,26 @@
 #! /bin/bash -x
-parttime=1;
-fulltime=2;
-emprateperhr=20;
-randomcheck=$((RANDOM%3));
-case $randomcheck in
-	$fulltime)
-		emphrs=8
+#constants
+full_time=1;
+part_time=2;
+emp_rate_per_hr=20;
+num_of_working_days=30
+total_salary=0
+#variable
+for ((day=1; day<=$num_of_working_days; day++))
+do
+	emp_check=$((RANDOM%3)) #generating a random number
+	case $emp_check in
+		$full_time)
+			emp_hrs=8
 		;;
-	$partime)
-		emphrs=4
+		$part_time)
+			emp_hrs=4
 		;;
-	*)
-		emphrs=0
+		*)
+			emp_hrs=0
 		;;
-esac
-salary=$(($emphrs*$emprateperhr));
-echo "salary of the employee is $salary";
+	esac
+wage=$(($emp_rate_per_hr*$emp_hrs));
+total_salary=$(($total_salary+wage));
+echo "$total_salary";
+done
